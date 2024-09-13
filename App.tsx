6992d01be4e7eafa729653faeb9ecffcae95aba1
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-09-13 11:19:27 +0800

    initiated todo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+
+type Task = {
+	id: number;
+	title: string;
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [tasks, setTasks] = useState<Task[]>([]);
+	const [taskTitle, setTaskTitle] = useState<string>('');
+
+	const addTask = () => {
+		if (taskTitle.trim().length === 0) return;
+	
+		const newTask: Task = {
+		  	id: Math.random(),
+		 	title: taskTitle.trim(),
+		};
+	
+		setTasks((prevTasks) => [...prevTasks, newTask]);
+		setTaskTitle('');
+	};
+	
+	const deleteTask = (id: number) => {
+		setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text>To-Do App</Text>
+			<View>
+				<TextInput
+					placeholder="Enter a task"
+					value={taskTitle}
+					onChangeText={setTaskTitle}
+				/>
+				<Button title="Add Task" onPress={addTask} />
+			</View>
+			<FlatList
+				data={tasks}
+				keyExtractor={(item) => item.id.toString()}
+				renderItem={({ item }) => (
+				<View>
+					<Text>{item.title}</Text>
+					<Button title="Delete" onPress={() => deleteTask(item.id)} />
+				</View>
+				)}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
